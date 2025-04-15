020bc6ae1913d396253a99350af78aeb5737c3bf
102guss <brown_321@live.com>
2025-04-15 15:24:08 -0600

    Estructura del proyecto

---
+++ mi-exploracion-espacial/planetas.js
@@ -0,0 +1,26 @@
+const planetas = [
+    {
+      nombre: "Titán",
+      descripcion: "La luna más grande de Saturno, con lagos de metano.",
+      descubiertoEn: "1655"
+    },
+    {
+      nombre: "Próxima CentauriA",
+      descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+      descubiertoEn: "2016"
+    },
+    {
+        nombre: "Planeta Marcioano",
+        descripcion: "Un planeta del sistema solar que tiene agua en su superficie.",
+        descubiertoEn: "2020"
+        
+      },
+      {
+        nombre: "Planeta  Virtrum",
+        descripcion: "Un exoplaneta donde habitan seres con super poderes similares a lo de supermán",
+        descubiertoEn: "2022"
+      }
+    
+  ];
+  
+  module.exports = planetas;
